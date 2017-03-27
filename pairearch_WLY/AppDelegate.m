a4f4c0c41dd9a14cf31ae396530ceaa85bb11a6c
Leo <Leo@LeodeAir.AirDream>
2017-03-27 13:51:54 +0800

    no message

--- pairearch_WLY/AppDelegate.m
+++ pairearch_WLY/AppDelegate.m
@@ -97,13 +97,13 @@
 
 //百度统计
 - (void)startBaiduMob {
-    //添加百度统计
     
+    //添加百度统计
     BaiduMobStat *statTracker = [BaiduMobStat defaultStat];
     statTracker.enableExceptionLog = YES; // 是否允许截获并发送崩溃信息，请设置YES或者NO
     statTracker.channelId = @"appStore";//设置您的app的发布渠道
     statTracker.sessionResumeInterval = 600;//设置应用进入后台再回到前台为同一次session的间隔时间[0~600s],超过600s则设为600s，默认为30s
-    statTracker.enableDebugOn = YES; //调试的时候打开，会有log打印，发布时候关闭
+    statTracker.enableDebugOn = NO; //调试的时候打开，会有log打印，发布时候关闭
     /*如果有需要，可自行传入adid
      NSString *adId = @"";
      if([[[UIDevice currentDevice] systemVersion] floatValue] >= 6.0f){
